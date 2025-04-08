78060fae4c9707eb00d26f2f84bf41cae7b0a910
Rstu <restualfazhuri@gmail.com>
2025-04-08 10:46:46 +0700

    Add files via upload

---
+++ pre-1(pengulangan)/for-loop.py
@@ -0,0 +1,28 @@
+# perulangan (loop)
+
+# for kondisi: JIKA KONDISI TERPENUHI 
+#      aksi  JALANKAN AKSI
+
+#Angka_list = [0,2,4,8,10] #this is list
+#print (Angka_list)
+
+# angka = 0
+# print(angka)
+# angka += 1 
+# print(angka)
+
+# for loop
+#for i in Angka_list:
+#    print(f"i sekarang -> {i}")
+#print("loop berakhir \n")
+
+for i in range(5):
+    print(f"ini yang ada di range = {i}")
+print("loop berakhir")
+
+for i in range(1,10):
+    print(f"ini yang ada di range = {i}")
+print("loop berakhir")
+
+for i in range(1,4):
+    print("*" * i)
